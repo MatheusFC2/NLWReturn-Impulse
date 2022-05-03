734d3888f08708161c85693b64c6d577955785d9
MatheusFC2 <matheusfreitascampos@gmail.com>
2022-05-02 21:45:54 -0300

    docs: Finished in the first class

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,9 +1,21 @@
 import { ChatTeardropDots } from 'phosphor-react'
+import { Popover } from '@headlessui/react'
 
 export function Widget() {
+
     return (
-        <button className="bg-violet-500 rounded-full px-3 h-12">
-            <ChatTeardropDots className="w-6 h-6"/>
-        </button>
+        <Popover className="absolute bottom-5 right-5">
+          <Popover.Panel>OlA</Popover.Panel> 
+
+            <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+                <ChatTeardropDots className="w-6 h-6"/>
+
+                <span className="max-w-0  overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+                    <span className="pl-3"></span>
+                    FeedBack
+                </span>
+            </Popover.Button>
+
+        </Popover>
     )
 }
